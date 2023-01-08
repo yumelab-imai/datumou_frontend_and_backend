c47a64dd0d06c49f9d558bb856659e1da6bb603b
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-01-08 21:27:02 +0900

    [2023年度] 新規開発 | 環境構築 | error原因特定のためのcommit(必要であれば後程git reset)

--- frontend/next_laravel_test/src/pages/api/hello.ts
+++ frontend/next_laravel_test/src/pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' })//status code を200にして、jsonデータを格納。403は、Forbidden error。ステータスコード401（Unauthorized）は適切な認証情報を持たずにリソースにアクセスしようとしたことを示します。
 }
