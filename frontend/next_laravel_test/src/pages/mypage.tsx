01ee3d9d9b6f1fd5481d529b1bc72d6db11ea574
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-01-15 12:58:18 +0900

    [2023年度] 新規開発 | 機能実装 | Mypage | Google Map API 導入 | 初期ページで『東京スカイツリー』へピンを立てる (*^^*)(正常に動作/_ok_rebuild)

--- frontend/next_laravel_test/src/pages/mypage.tsx
+++ frontend/next_laravel_test/src/pages/mypage.tsx
@@ -10,100 +10,168 @@ import React, { useEffect } from 'react';
 import { useHandleLogout } from '../components/shared/function/Auth/AuthLisnter'
 import { mapDatum } from '../components/atomic/Atoms/mapData'
 
-// import '../components/atomic/Atoms/css/stamprally/main.css';
-// import '../components/atomic/Atoms/css/stamprally/reset.css';
+import { GoogleMap as GoogleMapComponent, LoadScript } from '@react-google-maps/api';
+import { FC } from "react";
+import { useJsApiLoader, MarkerF, InfoWindowF } from "@react-google-maps/api";
+import { useState } from "react";
+
+// import  "../../.env";
+
+// When  you try to change Google Map Styles
+//  import { InterfaceMap } from "./GoogleMapStyles";
+//  const googleMapOptions = {
+//   styles: InterfaceMap,
+// };
 
 console.log({mapDatum})
 const handleLogout = useHandleLogout()
 
+type Props = {
+  defaultPosition: google.maps.LatLngLiteral;
+};
 
-const inter = Inter({ subsets: ['latin'] })
+const markerLabel: google.maps.MarkerLabel = {
+    text: "東京スカイツリー",
+    fontFamily: "sans-serif",
+    fontSize: "15px",
+    fontWeight: "bold",
+  };
+
+
+const GoogleMap: FC<Props> = (props) => {
+  const { map, zoom, isLoaded, onLoad } = useMap({
+    defaultPosition: props.defaultPosition,
+  });
+
+  const containerStyle = {
+    width: "100vw",
+    height: "75vh",
+  };
+  return (
+    <>
+      {isLoaded ? (
+        <GoogleMapComponent
+            // When  you try to change Google Map Styles
+            // options={googleMapOptions}
+            zoom={zoom}
+            mapContainerStyle={containerStyle}
+            onLoad={onLoad}
+        >
+            <MarkerF position={props.defaultPosition} label={markerLabel} />
+            {/* HTMLでの吹き出しを設置 */}
+            {/* <InfoWindowF position={props.defaultPosition}>
+                <>
+                <h3 >東京スカイツリー</h3>
+                </>
+            </InfoWindowF> */}
+        </GoogleMapComponent>
+      ) : (
+        "Now loading.."
+      )}
+    </>
+  );
+};
+
+type Map = google.maps.Map;
+export const useMap = ({ defaultPosition }: Props) => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map",
+    // NEXT_PUBLIC_にしないと、サーバ側での処理となるためクライアントで使用できなくなりエラーになる
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? 'NO API_KEY',
+  });
+  const [map, setMap] = useState<Map | null>(null);
+  const onLoad = (map: Map) => {
+    const bounds = new window.google.maps.LatLngBounds(defaultPosition);
+    map.fitBounds(bounds);
+    setMap(map);
+};
+const zoom: number = 15;
+
+  return { map, zoom, isLoaded, onLoad };
+};
+
+ 
+
 
 export default function Home() {
-  useEffect(() => {
+    useEffect(() => {
         //   axios.get("/api/books").then((res) => {
         //   const data = res.data;
         //   console.log(data);
         //  });
   }, []);
 
-//   function initMap() {
-//       var opts = {
-//         zoom: 15,
-//         center: new google.maps.LatLng(35.709984,139.810703)
-//       };
-//       var map = new google.maps.Map(document.getElementById("map"), opts);
-//     }
-
-    const initMap = (): void => {
-        let opts = {
-        zoom: 15,
-        center: new google.maps.LatLng(35.658594999999984,74540199999998)
-      };
-      let map = new google.maps.Map(document.getElementById("map"), opts);
-    }
-
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-      <div className="page page-course">
-    <div className="area_wrapper">
-      <div className="area area-course_header">
-        <div className="course_header">
-          <h1 className="title">{'医療脱毛クリニック'}</h1>
-        </div>
-      </div>
+        <Head>
+            <title>Create Next App</title>
+        </Head>
+        <div className="page page-course">
+            <div className="area_wrapper">
+                <div className="area area-course_header">
+                    <div className="course_header">
+                    <h1 className="title">{'医療脱毛クリニック'}</h1>
+                    </div>
+                </div>
+                {/* <Map/> */}
+                {/* JSでの処理 */}
+                {/* <div className="area area-map"> */}
+                    {/* <div className="map " style={{width: '400px', height:'100px'}}
+                    ></div> */}
+                {/* </div> */}
+                <GoogleMap
+                    defaultPosition={{
+                        lat: 35.70989699999999,
+                        lng: 139.81071400000002,
+                    }}
+                >
+                    {/*  */}
+                </GoogleMap>
 
-      <div className="area area-sub_controllers">
-        <div className="sub_controllers">
-          <div
-            className="button current_position_button"
-          >
-            現在地
-          </div>
-          <div
-            className="button watch_position_restart_button"
-          >
-            GPS更新
-          </div>
-        </div>
-      </div>
+                <div className="area area-sub_controllers">
+                    <div className="sub_controllers">
+                    <div
+                        className="button current_position_button"
+                    >
+                        現在地
+                    </div>
+                    <div
+                        className="button watch_position_restart_button"
+                    >
+                        GPS更新
+                    </div>
+                    </div>
+                </div>
 
-      <div className="area area-current_stamp_info">
-          <div className="current_stamp_info">
-            <span className="text">検索ヒット数：{'999'}</span>
-          </div>
-      </div>
+                <div className="area area-current_stamp_info">
+                    <div className="current_stamp_info">
+                        <span className="text">検索ヒット数：{'51'}店舗</span>
+                    </div>
+                </div>
 
-        <div className="area area-spot_list">
-            
+                <div className="area area-spot_list">
+                    <div className="area area-stamp_button">
 
-        <div className="area area-stamp_button">
-
-            <div className="stamp_button active">
-            <span className="text">指定のクリニック検索</span>
+                        <div className="stamp_button active">
+                        <span className="text">指定のクリニック検索</span>
+                        </div>
+                        <div className="stamp_button">
+                        <span className="text">半径〇〇キロ内検索</span>
+                        </div>
+                        <div className="stamp_button active">
+                        <span className="text">サンプルテキスト</span>
+                        </div>
+                    </div>
+                    {/* Google Map */}
+                            <div className="area area-map">
+                                <div className="map" 
+                                ></div>
+                            </div>
+                    {/* <Map/> */}
+                </div>
             </div>
-            <div className="stamp_button">
-            <span className="text">半径〇〇キロ内検索</span>
-            </div>
-            <div v-else className="stamp_button active">
-            <span className="text">サンプルテキスト</span>
-            </div>
-
         </div>
-
-        {/* Google Map */}
-      <div className="area area-map">
-        <div className="map" 
-        ></div>
-      </div>
-    </div>
-  </div>
-  </div>
-
-
     </>
   )
+  
 }
